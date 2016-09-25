f16b77bd900c923409771afe3455409e905fdeb7
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-09-25 15:30:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Suncoast.js
+
+## Higher Order Components in React and Node Internet of Rain (October 4th, 2016)
---
+++ presentation/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+  Spectacle,
+  Deck,
+  Slide,
+  Heading
+} from 'spectacle'
+
+export default function Presentation () {
+  return (
+    <Spectacle>
+      <Deck>
+        <Slide>
+          <Heading>
+            Higher Order Components in React and Node Internet of Rain
+          </Heading>
+        </Slide>
+      </Deck>
+    </Spectacle>
+  )
+}
