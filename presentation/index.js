02b9f4b018093ca811df6f3b8902e1b5a10ea2b1
Adam Recvlohe <adam.recvlohe@gmail.com>
2016-09-25 16:21:46 -0400

    Split into two parts (#3)

--- presentation/index.js
+++ presentation/index.js
@@ -11,10 +11,45 @@ export default function Presentation () {
     <Spectacle>
       <Deck>
         <Slide>
+          <Heading>Suncoast.js</Heading>
+        </Slide>
+        <Slide>
           <Heading>
             Higher Order Components in React and Node Internet of Rain
           </Heading>
         </Slide>
+        {/* }
+            /////////////////////////////////
+            ///////  START: PART ONE ///////
+            ////////////////////////////////
+        { */}
+        <Slide>
+          <Heading>PART 1</Heading>
+        </Slide>
+        <Slide>
+          <Heading>Higher Order Components in React</Heading>
+        </Slide>
+        {/* }
+            //////////////////////////////
+            ///////  END: PART ONE ///////
+            //////////////////////////////
+        { */}
+        {/* }
+            ////////////////////////////////
+            ///////  START: PART TWO ///////
+            ////////////////////////////////
+        { */}
+        <Slide>
+          <Heading>PART 2</Heading>
+        </Slide>
+        <Slide>
+          <Heading>Node Internet of Rain</Heading>
+        </Slide>
+        {/* }
+            //////////////////////////////
+            ///////  END: PART TWO ///////
+            //////////////////////////////
+        { */}
       </Deck>
     </Spectacle>
   )
